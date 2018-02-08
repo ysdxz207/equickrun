5ebb87cb4f962f8d3569f56fe9c161470306dd81
ysdxz207 <ysdxz207@qq.com>
2018-02-09 00:33:33 +0800

    11

--- renderer.js
+++ renderer.js
@@ -2,3 +2,8 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+var header = document.querySelector('#header');
+
+header.onmousemove = function (e) {
+    console.log(e.clientX,e.clientY)
+}
