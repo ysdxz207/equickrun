da45900474f5c4993b6925bb8a4f920d7cfe5134
Moses <ysdxz207@qq.com>
2018-02-09 15:14:20 +0800

    a

--- renderer.js
+++ renderer.js
@@ -1,17 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
+const windowsShortcuts = require('windows-shortcuts');
 const electron = require('electron')
 const remote = electron.remote
 const mainWindow = remote.getCurrentWindow()
 
-//拖动窗口 start
 var header = document.querySelector('.header');
+var main = document.querySelector('.main');
+
+//拖动窗口 start
 var x,y;
 
 header.onmousedown = function (e) {
-    console.log('start',e.clientX,e.clientY)
     x = e.clientX
     y = e.clientY
 }
@@ -26,10 +27,39 @@ header.onmousemove = function (e) {
     }
 }
 
-header.onmouseup = function () {
+document.onmouseup = function () {
     x = undefined;
     y = undefined;
 }
 //拖动窗口 end
 
 
+//拖放文件进列表 start
+
+//阻止浏览器默认行。
+document.addEventListener('drop', function (e) {
+    e.preventDefault()
+})
+document.addEventListener('dragleave', function (e) {
+    e.preventDefault()
+})
+document.addEventListener('dragenter', function (e) {
+    e.preventDefault()
+})
+document.addEventListener('dragover', function (e) {
+    e.preventDefault()
+})
+main.addEventListener('drop', onDropFiles)
+
+
+function onDropFiles(e) {
+    var fileList = e.dataTransfer.files
+
+    for (var i = 0; i < fileList.length; i ++) {
+        let file = fileList[i]
+        let name = file.name
+        let path = file.path
+
+        windowsShortcuts.query(path, console.log)
+    }
+}
